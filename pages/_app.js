2e65ab1a9f082e021265556f7da19f96544590a9
NetherMC <thelio.trinite@outlook.com>
2021-06-14 20:51:57 +0200

    projet

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>ToolBot</title>
+        <meta name="title" content="ToolBot" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
